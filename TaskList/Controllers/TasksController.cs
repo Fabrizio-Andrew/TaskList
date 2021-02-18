01706ee0203305ae530c2c19f07262d69c19374e
Andrew <andrewdfabrizio@gmail.com>
2021-02-18 14:19:52 -0500

    API Get requests established - Swagger UI not working

--- TaskList/Controllers/TasksController.cs
+++ TaskList/Controllers/TasksController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using TaskList.DataTransferObjects;
+using TaskList.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,8 @@ using System.Threading.Tasks;
 
 namespace TaskList.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Produces("application/json")]
     [ApiController]
     public class TasksController : ControllerBase
     {
@@ -18,17 +21,41 @@ namespace TaskList.Controllers
         /// <summary>
         /// The database of tasks.
         /// </summary>
-        private static Dictionary<int, Task> _tasks = new Dictionary<int, Task>();
+        private static Dictionary<int, DataTransferObjects.Task> _tasks = new Dictionary<int, DataTransferObjects.Task>();
+
+        /// <summary>
+        /// Logger instance
+        /// </summary>
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomersController"/> class.
+        /// </summary>
+        public TasksController(ILogger<TasksController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        private static DateTime firstdate = DateTime.Parse("2021-02-03");
+        static TasksController()
+        {
+            // Initialize Data
+            _tasks.Add(1, new DataTransferObjects.Task() { id = 1, taskName = "Buy groceries", isCompleted = false, dueDate = DateTime.Parse("2021-02-03") });
+            _tasks.Add(2, new DataTransferObjects.Task() { id = 2, taskName = "Workout", isCompleted = true, dueDate = DateTime.Parse("2021-01-01") });
+            _tasks.Add(3, new DataTransferObjects.Task() { id = 3, taskName = "Paint fence", isCompleted = false, dueDate = DateTime.Parse("2021-03-15") });
+            _tasks.Add(4, new DataTransferObjects.Task() { id = 4, taskName = "Mow Lawn", isCompleted = false, dueDate = DateTime.Parse("2021-06-11") });
+        }
 
         // POST api/<TasksController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
         // GET: api/<TasksController>
         [HttpGet]
-        [ProducesResponseType(typeof(Task), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(DataTransferObjects.Task), StatusCodes.Status200OK)]
         [Route("api/v1/tasks")]
         public IActionResult Get()
         {
@@ -37,13 +64,30 @@ namespace TaskList.Controllers
 
         // GET api/<TasksController>/5
         [HttpGet("{id}")]
-        [ProducesResponseType(typeof(Task), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(DataTransferObjects.Task), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
         [ProducesResponseType(typeof(void), StatusCodes.Status500InternalServerError)]
         [Route("api/v1/tasks/{id}")]
         public IActionResult Get(int id)
         {
-            return "value";
+            try
+            {
+                if (id < 1)
+                {
+                    throw new Exception("Invalid ID.");
+                }
+                return new ObjectResult(_tasks[id]);
+            }
+            catch (KeyNotFoundException knfEx)
+            {
+                _logger.LogInformation(LoggingEvents.GetItem, knfEx, "CustomerController Customer(id=[{id}]) was not found.", id);
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(LoggingEvents.InternalError, ex, "CustomerControlle Customer(id=[{id}]) caused an internal error.", id);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
--- TaskList/Startup.cs
+++ TaskList/Startup.cs
@@ -7,9 +7,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 
 namespace TaskList
 {
@@ -26,11 +31,56 @@ namespace TaskList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // DEMO: Entity Framework: Register the context with dependency injection
+            //services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Setup swagger 
+            SetupSwaggerDocuments(services);
+
+            // DEMO: Setup custom settings
+            //SetupCustomSettings(services);
+
+            // Setup the application insights integration
+            //SetupApplicationInsights(services);
+        }
+
+        /// <summary>
+        /// Sets up the swagger documents
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        private static void SetupSwaggerDocuments(IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Task List API",
+                    Version = "v1",
+                    Description = "Task List API",
+                });
+
+                // Use method name as operationId so that ADD REST Client... will work
+                c.CustomOperationIds(apiDesc =>
+                {
+                    return apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : null;
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+
+            SetupSwaggerJsonGeneratgionAndUI(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -45,6 +95,30 @@ namespace TaskList
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        /// <summary>
+        /// Sets up the Swagger JSON file and Swagger Interactive UI
+        /// </summary>
+        /// <param name="app">The application builder</param>
+        private static void SetupSwaggerJsonGeneratgionAndUI(IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c =>
+            {
+                // Use the older 2.0 format so the ADD REST Client... will work
+                c.SerializeAsV2 = true;
+            });
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            //       specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task List API");
+
+                // Serve the Swagger UI at the app's root (http://localhost:<port>)
+                c.RoutePrefix = string.Empty;
             });
         }
     }
